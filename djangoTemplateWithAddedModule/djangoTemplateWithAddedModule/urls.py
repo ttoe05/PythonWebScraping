be92b779553f695063f60b855869eb528a11bda2
Christopher Adams <chrismichaeladams@gmail.com>
2018-09-03 17:30:30 -0400

    Update urls.py to work with later versions of Django

--- djangoTemplateWithAddedModule/djangoTemplateWithAddedModule/urls.py
+++ djangoTemplateWithAddedModule/djangoTemplateWithAddedModule/urls.py
@@ -3,11 +3,12 @@ from django.contrib import admin
 
 # Import the view:
 from . import views
+from addedModule.views import addedModule
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.home),
     url(r'^about', views.about),
-    url(r'', 'addedModule.views.addedModule')
+    url(r'', addedModule)
 
 ]
